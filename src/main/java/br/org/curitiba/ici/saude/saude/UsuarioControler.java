16b766079e0645afea36193ce0fa8f831e500485
Marcio Wielganczuk <marcio@Marcios-MacBook-Air.local>
2023-08-15 22:06:48 -0300

    adicionado novo metodo no servico usuario

--- src/main/java/br/org/curitiba/ici/saude/saude/UsuarioControler.java
+++ src/main/java/br/org/curitiba/ici/saude/saude/UsuarioControler.java
@@ -1,5 +1,9 @@
 package br.org.curitiba.ici.saude.saude;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping
 public class UsuarioControler {
 
+  public void getUsuarios() {}
 }

1e954635720e0bb49792daa55570ac20078a739f
Marcio Wielganczuk <marcio@Marcios-MacBook-Air.local>
2023-08-15 22:50:00 -0300

    correcos de conflitos

--- src/main/java/br/org/curitiba/ici/saude/saude/UsuarioControler.java
+++ src/main/java/br/org/curitiba/ici/saude/saude/UsuarioControler.java
@@ -1,9 +1,12 @@
 package br.org.curitiba.ici.saude.saude;
 
+
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping
 public class UsuarioControler {
 
   public void getUsuarios() {}
+
+
 }
